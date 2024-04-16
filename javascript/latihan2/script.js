c1d6166f532c3f4057d6e95bb72636d5e9738761
rqz234 <160827712+rqz234@users.noreply.github.com>
2024-04-16 16:58:42 +0700

    update

--- javascript/latihan2/script.js
+++ javascript/latihan2/script.js
@@ -1,87 +1,42 @@
-let data = ["a", "b", "c"];
-let listMhs = document.getElementById('listMhs');
+let inputnama = document.getElementById("nama");
+let inputtestimoni = document.getElementById("testimoni");
+let btnsimpan = document.getElementById("btnSimpan");
+let list = document.getElementById("list");
 
-data.forEach(showMahasiswa);
+let data1 = []
 
-function showMahasiswa(value, index){
-    console.log(value);
-    // listMhs.innerHTML = "<li>" + value + "</li>";
-    listMhs.innerHTML += `<li>${value}</li>`;
+btnsimpan.addEventListener("click", function(){
+    if(localStorage.getItem("testimoni")){
+        data1 = JSON.parse(localStorage.getItem("testimoni"));
+        data1.push({
+            nama: inputnama.value,
+            testimoni : inputtestimoni.value,
+        });
+    }
+    else{
+        data1.push({
+            nama : inputnama.value,
+            testimoni : inputtestimoni.value,
+        })
+    }
+    localStorage.setItem("testimoni", JSON.stringify(data1))
 
+    // Panggil GetData()
+    getData();
+})
+
+// tampilkan data localStorage
+function getData(){
+    // Clear Element List
+    list.innerHTML = "";
+    if(localStorage.getItem("testimoni")){
+        // ada localstorage dengan key testimoni
+        data = JSON.parse(localStorage.getItem("testimoni"));
+        for (const [index, row] of data.entries){
+            list.innerHTML += `<li>${row.nama} ${row.testimoni}</li>`;
+        }
+    }
 }
-
-
-let npm = document.getElementById("npm");
-npm.innerHTML = localStorage.getItem("npm");
-
-let nama = document.getElementById("nama");
-nama.innerHTML = localStorage.getItem("nama");
-
-// Simpan data ke localStorage
-localStorage.setItem("email", "ahmad@test.com");
-
-// Data Array
-let hobi = ["Baca", "Game", "Coding"];
-
-localStorage.setItem("hobi", hobi);
-localStorage.setItem("myhobi", JSON.stringify(hobi));   
-
-// Data dalam bentuk object
-let mhs = {"npm" : 2226240103, "nama" : "Donok"};
-localStorage.setItem("mhs", mhs)
-
-console.log(mhs.npm);
-console.log(mhs.nama);
-
-// Kombinasi array dan object
-let nilai = [
-    {"kode_mk" : "SI0001", "nama_mk" : "Pemrograman Web"}, 
-    {"kode_mk" : "SI0002", "nama_mk" : "Pengembangan Aplikasi Bergerak"},
-];
-
-// Tampilkan nama_mk pemrograman web
-console.log(nilai[0].nama_mk);
-
-// Tampilkan nama_mk Pengembangan Aplikasi Bergerak
-console.log(nilai[1].nama_mk);
-
-// simpan nilai ke dalam locaStorage
-localStorage.setItem("nilai", JSON.stringify(nilai));
-
-// tampilkan menggunakan for atau forEach
-let lsNilai = JSON.parse(localStorage.getItem("nilai"));
-for (const [index, data] of lsNilai.entries()){
-    console.log(data.kode_mk);
-    console.log(data.nama_mk);
-
-    // tampilkan data mk ke dalam id = "listMk">
-    document.getElementById("listMk").innerHTML += `<li>${data.kode_mk} ${data.nama_mk}</li>`
-
-}
-
-let nama_kamu = "";
-let testimoni_kamu = "";
-let simpan = "";
-
-
-function print(){
-    
-    nama_kamu = document.getElementById("nama_kamu").value
-    testimoni_kamu = document.getElementById("testimoni_kamu").value
-    simpan = document.getElementById("simpan").value;
-
-    localStorage.getItem("nama_kamu");
-    localStorage.getItem("testimoni_kamu");
-    localStorage.getItem("simpan");
-
-    simpan.push(nama_kamu, testimoni_kamu)
-    document
-
-}
-
-
-
-
 
 
 
